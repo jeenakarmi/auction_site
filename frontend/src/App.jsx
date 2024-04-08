3b909d69b8def6f8aa469a5e9a3f786cb6813154
nimeshakya <nimeshakyabtw@gmail.com>
2024-04-08 23:34:18 +0545

    Login / Register page complete.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Stack } from '@chakra-ui/react';
 import { useGlobalContext } from './context/GlobalContext';
 
 import NavBar from './components/NavBar/NavBar';
@@ -10,19 +11,27 @@ import RegisterForm from './components/RegisterForm/RegisterForm';
 import Browse from './pages/Browse';
 
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 const App = () => {
     const { currentUser } = useGlobalContext();
 
     return (
-        <>
+        <Stack
+            width={'100%'}
+            minH={'100vh'}
+            direction={'column'}
+            align={'center'}
+            justify={'flex-start'}
+        >
             <NavBar />
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
                 <Route path='/login' element={<LoginForm />} />
                 <Route path='/register' element={<RegisterForm />} />
             </Routes>
-        </>
+        </Stack>
     );
 };
 
--- frontend/src/components/RegisterForm/RegisterForm.jsx
+++ frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -11,6 +11,8 @@ import {
     Stack,
     Button,
     Text,
+    Box,
+    Heading,
     Link as ChakraLink,
 } from '@chakra-ui/react';
 
@@ -36,70 +38,97 @@ const RegisterForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <FormControl>
-                <FormLabel>Email:</FormLabel>
-                <Input
-                    type='email'
-                    name='email'
-                    placeholder='email@gmail.com'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel>Password:</FormLabel>
-                <Input
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel>Username:</FormLabel>
-                <Input
-                    type='text'
-                    name='username'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel>Phone No.:</FormLabel>
-                <Input
-                    type='number'
-                    name='phone'
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel>User Type:</FormLabel>
-                <RadioGroup defaultValue={userType}>
-                    <Stack direction={'row'}>
-                        <Radio
-                            value='BUYER'
-                            onChange={(e) => setUserType(e.target.value)}
-                        >
-                            BUYER
-                        </Radio>
-                        <Radio
-                            value='SELLER'
-                            onChange={(e) => setUserType(e.target.value)}
-                        >
-                            SELLER
-                        </Radio>
-                    </Stack>
-                </RadioGroup>
-            </FormControl>
-            <Button type='submit' colorScheme='blue'>
-                Register
-            </Button>
-            <ChakraLink as={ReactRouterLink} to={'/login'}>
-                Have an account? Log in instead!
-            </ChakraLink>
-        </form>
+        <Box
+            width={'100%'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            flex={'1 0 auto'}
+        >
+            <Stack
+                width={'30%'}
+                rounded={10}
+                padding={10}
+                direction={'column'}
+                gap={4}
+                boxShadow={'0 0 30px 0 #38664150'}
+            >
+                <Heading marginBottom={10} color={'#1f1f1f'}>
+                    Sign Up
+                </Heading>
+                <form
+                    onSubmit={handleSubmit}
+                    className='flex flex-col gap-4 justify-center items-center'
+                >
+                    <FormControl>
+                        <FormLabel>Email:</FormLabel>
+                        <Input
+                            type='email'
+                            name='email'
+                            placeholder='email@gmail.com'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Password:</FormLabel>
+                        <Input
+                            type='password'
+                            name='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Username:</FormLabel>
+                        <Input
+                            type='text'
+                            name='username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Phone No.:</FormLabel>
+                        <Input
+                            type='number'
+                            name='phone'
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>User Type:</FormLabel>
+                        <RadioGroup defaultValue={userType}>
+                            <Stack direction={'row'} gap={10}>
+                                <Radio
+                                    value='BUYER'
+                                    onChange={(e) =>
+                                        setUserType(e.target.value)
+                                    }
+                                >
+                                    BUYER
+                                </Radio>
+                                <Radio
+                                    value='SELLER'
+                                    onChange={(e) =>
+                                        setUserType(e.target.value)
+                                    }
+                                >
+                                    SELLER
+                                </Radio>
+                            </Stack>
+                        </RadioGroup>
+                    </FormControl>
+                    <Button type='submit' colorScheme='blue' width={'100%'}>
+                        Sign Up
+                    </Button>
+                    <ChakraLink as={ReactRouterLink} to={'/login'}>
+                        Have an account? Log in instead!
+                    </ChakraLink>
+                </form>
+            </Stack>
+        </Box>
     );
 };
 
