5ce8c7631e3c646208b819c4e676f3b653a30a8d
Sujalcha7 <sujalkoju97@gmail.com>
2024-04-14 18:53:53 +0545

    car_data

---
+++ frontend/src/data/car_data.jsx
@@ -0,0 +1,47 @@
+export const car_data = [
+    {
+        brandName: 'brandname',
+        models: [
+            {
+                modelName: 'SUV',
+                vechicles: [
+                    {
+                        vehicleName: 'elevate',
+                        type: ['SVMT', 'VMT']
+                    },
+                    {
+                        vehicleName: 'passport',
+                        type: ['SVMT', 'VMT']
+                    }
+                ],
+            },
+            {
+                modelName: 'Convertible',
+                vechicles: [
+                    {
+                        vehicleName: 'convertible1',
+                        type: ['SVMT', 'VMT']
+                    }
+                ], 
+            },
+        ],
+    },
+    {
+        brandName: 'toyota',
+        models: [
+            {
+                modelName: 'modelName',
+                types: ['type1', 'type2'],
+            },
+        ],
+    },
+    {
+        brandName: 'tesla',
+        models: [
+            {
+                modelName: 'modelName',
+                types: ['type1', 'type2'],
+            },
+        ],
+    },
+];
