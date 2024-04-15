b1268c04d594135ad941e80ca5d19e14f6a470ce
nimeshakya <nimeshakyabtw@gmail.com>
2024-04-15 19:29:25 +0545

    Created link to profile page.

--- frontend/src/pages/User/userprofile.jsx
+++ frontend/src/pages/User/userprofile.jsx
@@ -4,11 +4,11 @@ import './userprofile.css';
 import { useGlobalContext } from '../../context/GlobalContext';
 
 const UserProfile = () => {
-    const {currentUser} = useGlobalContext();
+    const { currentUser } = useGlobalContext();
     const [userData, setUserData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-  
+
     // useEffect(() => {
     //   const fetchUserData = async () => {
     //     try {
@@ -20,60 +20,59 @@ const UserProfile = () => {
     //       setLoading(false);
     //     }
     //   };
-    
+
     //   fetchUserData();
     // }, [currentUser]);s
-    
-  
+
     // if (loading) {
     //   return <div>Loading...</div>;
     // }
-  
+
     // if (error) {
     //   return <div>Error: {error}</div>;
     // }
-    console.log('currentUser:', currentUser);
-  
-  return (
-    <div className="user-profile">
-      <h2>Personal and Account Information</h2>
-      <div className="detail">
-        <label>Username:</label>
-        <p>{currentUser.username}</p>
-      </div>
-      <div className="separator"></div>
-      <div className="detail">
-        <label>User ID:</label>
-        <p>{currentUser.id}</p>
-      </div>
-      {/* <div className="separator"></div>
+    console.log(currentUser);
+
+    return (
+        <div className='user-profile'>
+            <h2>Personal and Account Information</h2>
+            <div className='detail'>
+                <label>Username:</label>
+                <p>{currentUser.username}</p>
+            </div>
+            <div className='separator'></div>
+            <div className='detail'>
+                <label>User ID:</label>
+                <p>{currentUser.id}</p>
+            </div>
+            {/* <div className="separator"></div>
       <div className="detail">
         <label>Password:</label>
         <p>{userData.password}</p>
       </div> */}
-      <div className="separator"></div>
-      <div className="detail">
-        <label>Email:</label>
-        <p>{currentUser.email}</p>
-      </div>
-      <div className="separator"></div>
-      <div className="detail">
-        <label>Phone:</label>
-        <p>{currentUser.phone}</p>
-      </div>
-      <div className="separator"></div>
-      <div className="detail">
-        <label>User Type:</label>
-        <p>{currentUser.userType}</p>
-      </div>
-      {/* <div className="separator"></div>
+            <div className='separator'></div>
+            <div className='detail'>
+                <label>Email:</label>
+                <p>{currentUser.email}</p>
+            </div>
+            <div className='separator'></div>
+            <div className='detail'>
+                <label>Phone:</label>
+                <p>{currentUser.phone}</p>
+            </div>
+            <div className='separator'></div>
+            <div className='detail'>
+                <label>User Type:</label>
+                <p>{currentUser.userType}</p>
+            </div>
+            {/* <div className="separator"></div>
       <div className="detail">
         <label>Total Trades:</label>
         <p>{userData.totalTrades}</p>
       </div> */}
-      <div className="separator"></div>
-    </div>
-  );
+            <div className='separator'></div>
+        </div>
+    );
 };
 
 export default UserProfile;
