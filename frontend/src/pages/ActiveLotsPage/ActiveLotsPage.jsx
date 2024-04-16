712645bba8a11fb304f0ab0db2e85a50abe7acfd
nimeshakya <nimeshakyabtw@gmail.com>
2024-04-17 00:11:39 +0545

    Seller actions created in api.

---
+++ frontend/src/pages/ActiveLotsPage/ActiveLotsPage.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import {
+    Stack,
+    VStack,
+    Heading,
+    Text,
+    Link as ChakraLink,
+} from '@chakra-ui/react';
+
+import { useGlobalContext } from '../../context/GlobalContext';
+
+import BidItem from '../../components/BidItem/BidItem';
+const ActiveLotsPage = () => {
+    const { client } = useGlobalContext();
+
+    const [placedTopBids, setPlacedTopBids] = useState([]);
+
+    const getPlacedTopBids = () => {
+        client
+            .get('/api/items/seller-active-lots')
+            .then((res) => {
+                setPlacedTopBids(res.data.bidLots);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        getPlacedTopBids();
+    }, []);
+
+    return (
+        <Stack
+            direction={'column'}
+            maxW={'1280px'}
+            w={'80%'}
+            alignItems={'center'}
+            gap={10}
+            marginY={10}
+        >
+            <Heading size={'lg'}>Your Placed Top Bids</Heading>
+            <Stack
+                w={'100%'}
+                direction={'row'}
+                gap={5}
+                justifyContent={'center'}
+                flexWrap={'wrap'}
+            >
+                {placedTopBids.map((placedTopBid) => {
+                    return (
+                        <BidItem
+                            key={placedTopBid.id}
+                            placedTopBid={placedTopBid}
+                        />
+                    );
+                })}
+            </Stack>
+        </Stack>
+    );
+};
+
+export default ActiveLotsPage;
